524b6d3eb79e5d0f42c7103529d7e74e53718541
James Tan <james_tan97@outlook.com>
2020-05-22 11:37:40 +0800

    Disable unsubscription of other listeners to AR statements

--- lib/lograge/sql/extension.rb
+++ lib/lograge/sql/extension.rb
@@ -45,8 +45,8 @@ else
   Lograge::LogSubscribers::ActionController.prepend Lograge::Sql::Extension
 end
 
-ActiveSupport::LogSubscriber.log_subscribers.each do |subscriber|
-  Lograge.unsubscribe(:active_record, subscriber) if subscriber.is_a?(ActiveRecord::LogSubscriber)
-end
+#ActiveSupport::LogSubscriber.log_subscribers.each do |subscriber|
+#  Lograge.unsubscribe(:active_record, subscriber) if subscriber.is_a?(ActiveRecord::LogSubscriber)
+#end
 
 Lograge::ActiveRecordLogSubscriber.attach_to :active_record
